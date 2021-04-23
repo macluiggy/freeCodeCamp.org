30e4d2d8e693315d1c04fee7a29c4451bba1ed80
Luiggy Macias <ferrinluiggy@gmail.com>
2021-04-23 10:42:40 -0500

    updated

--- javascript/Regular Expressions/08. Match Single Character with Multiple Possibilities.js
+++ javascript/Regular Expressions/08. Match Single Character with Multiple Possibilities.js
@@ -1,13 +1,26 @@
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]./gi; // Change this line
+let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
 console.log(result);
 console.log(result.length);
 
 let names = ['Angel', 'Fernando', 'Angulo', 'Coveña'];
 let namesAn = []
-for(let i = 0, length1 = names.length; i < length1; i++){
-	const name = names[i];
-	namesAn.push(name.match(/an/gi));
+for (let i = 0, length1 = names.length; i < length1; i++) {
+    const name = names[i];
+    namesAn.push(name.match(/[an]/gi));
 }
 console.log(namesAn)
+
+//Verifica si el la primera palabra de cada nombre empieza con X letra
+let nombresQueEmpiezanConXLetra = [];
+for (let i = 0, length1 = names.length; i < length1; i++) {
+    const name = names[i];
+    if (/a/i.test(name[0])) {
+        // statement
+        nombresQueEmpiezanConXLetra.push(name);
+
+    }
+}
+console.log(nombresQueEmpiezanConXLetra);
+
---
+++ javascript/Regular Expressions/10. Match Numbers and Letters of the Alphabet.js
@@ -0,0 +1,25 @@
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[2-6h-s]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+console.log(result.length);
+
+//fucnion que debuelve la cantidad de vocales y numeros
+let string = 'soy un string 12 3 4 1';
+let regex = /[aeiou0-9]/g
+
+let cantidadVocalesYNumeros = () => {
+	let result = string.match(regex);
+	return result.length;
+}
+console.log(cantidadVocalesYNumeros())
+
+//fucnion que debuelve la cantidad de vocales y numeros y la palabra resultante
+let cantidadVocalesYNumerosConLaPalabra = () => {
+	let result = string.match(regex);
+	let palabra = '';
+	for(let i = 0, length1 = result.length; i < length1; i++){
+		palabra += result[i];
+	}
+	return `La cantidad de caracteres es ${result.length} y la palabra que se retorna es ${palabra} `;
+}
+console.log(cantidadVocalesYNumerosConLaPalabra());
