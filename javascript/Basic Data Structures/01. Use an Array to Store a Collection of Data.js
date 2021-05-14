bcb5ed4200efebb2157f5bf786f7ee012793d974
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-14 08:51:36 -0500

    empezando con data structures

---
+++ javascript/Basic Data Structures/01. Use an Array to Store a Collection of Data.js
@@ -0,0 +1,3 @@
+
+let yourArray = ['string', 0, 5==5, 1, 'end']; // Change this line
+console.log(yourArray)
---
+++ javascript/Regular Expressions/01. Use the JavaScript Console to Check the Value of a Variable.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.clear()
+console.log(sumAB);
+console.log(a)
+console.log('Hola')
