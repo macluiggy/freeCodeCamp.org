e264cb60086e8902a3ad99aba3760c47a484f79f
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-15 10:15:41 -0500

    updated

---
+++ javascript/Basic Data Structures/14. Add Key-Value Pairs to JavaScript Objects.js
@@ -0,0 +1,12 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+let stwbrs = 'strawberries'
+// Only change code below this line
+foods.bananas = 13, foods['grapes'] = 35, foods[stwbrs] = 27
+// Only change code above this line
+
+console.log(foods);
