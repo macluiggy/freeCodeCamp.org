bb329804628df922942ae28204f428e0cd697458
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-18 14:54:55 -0500

    usando react con css

--- Front end libraries/React/aprendiendo-react/src/components/Task.js
+++ Front end libraries/React/aprendiendo-react/src/components/Task.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import './Task.css';
 
 class Task extends Component {
 	render() {
-		return <div>
-        {this.props.task.title} - 
-        {this.props.task.description} - 
-        {this.props.task.done} - 
-        {this.props.task.id}
+		console.log(this.props)
+		const {id, title, description, done} = this.props.task;
+		let border = {border: 'solid'}
+		return <div className="yellow" style={border}>
+        <p className="title">{title}</p> - 
+        <p>{description}</p> - 
+        <p>{done}</p> - 
+        <p>{id}</p>
         <input type="checkbox"/>
         <button>x</button>
 		</div>
