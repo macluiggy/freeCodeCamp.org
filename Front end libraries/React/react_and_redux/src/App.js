14a9fda50e6d35d7c3192919eee58e64108100af
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-23 15:11:27 -0500

    using combineReducer

--- Front end libraries/React/react_and_redux/src/App.js
+++ Front end libraries/React/react_and_redux/src/App.js
@@ -2,58 +2,17 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import { createStore } from 'redux'
-
-
-const defaultState = {
-  authenticated: false
-};
-
-const authReducer = (state = defaultState, action) => {
-
-  switch (action.type) {
-    case 'LOGIN': 
-      return {
-        authenticated: true
-      }
-    case 'LOGOUT': 
-      return {
-        authenticated: false
-      }
-
-    default:
-      return state;
-
-  }
-
-};
-
-const store = createStore(authReducer);
-
-const loginUser = () => {
-  return {
-    type: 'LOGIN'
-  }
-};
-
-const logoutUser = () => {
-  return {
-    type: 'LOGOUT'
-  }
-};
-store.dispatch(logoutUser())
-console.log(store.getState())
-console.log(store.getState().authenticated)
+import Redux, { createStore } from 'react-redux'
+//components
+import Redux1 from './components/Redux1'
 function App() {
-
-  useEffect(() => {
-    console.log(store.getState().authenticated)
-  }, [store.getState().authenticated])
   return (
-    <div className="App">
-      <button onClick={() => store.dispatch(loginUser())} >LOGIN</button>
-      <button onClick={() => store.dispatch(logoutUser())} >LOGOUT</button>
-    </div>
+    <>
+  {/*
+    <Counter />
+  */}
+      < Redux1 />
+    </>
   );
 }
 
