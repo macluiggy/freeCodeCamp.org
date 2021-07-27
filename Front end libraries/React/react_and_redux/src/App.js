8d414d426cd04ddbb88b00f20862a43714be8b6f
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-27 11:42:45 -0500

    djjd

--- Front end libraries/React/react_and_redux/src/App.js
+++ Front end libraries/React/react_and_redux/src/App.js
@@ -4,14 +4,18 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Redux, { createStore } from 'react-redux'
 //components
-import Redux1 from './components/Redux1'
+//import Redux1 from './components/Redux1.jsx'
+import Redux2 from './components/Redux2.jsx'
 function App() {
   return (
     <>
   {/*
     <Counter />
   */}
-      < Redux1 />
+      {/*
+        < Redux1 />
+      */}
+      <Redux2 />
     </>
   );
 }
