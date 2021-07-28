c9d5d55e77c38d7d9d55e57faf1064e990e771dd
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-28 09:24:35 -0500

    djdjd

--- Front end libraries/React/react_and_redux/src/components/Contador.jsx
+++ Front end libraries/React/react_and_redux/src/components/Contador.jsx
@@ -5,53 +5,85 @@ import { createStore } from 'redux';
 //redux
 //empezamos con la accion que usara el dispatch
 const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
 const increment = () => {
 	return {
 		type: INCREMENT,
 	}
 }
+const reset = () => {
+	return {
+		type: RESET,
+	}
+}
 //ahora con el reductor
 const incrementReducer = (state = 0, action) => {
-	return {
-		INCREMENT: state+1,
+	switch (action.type) {
+		case INCREMENT:
+			return state + 1;
+		case DECREMENT:
+			return state - 1;
+		case RESET:
+			return 0
+		default:
+			return state;
 	}
 }
 //ahora creamos el store
 const store = createStore(incrementReducer);
 
 //react
-const Comp = ({ state, increaseValue }) => {
-	//const [state, setState] = useState(0) 
+const Comp = (props) => {
+	//const [state, setState] = useState() 
+	//console.log(props)
+	const inc = () => {
+		props.increaseValue()
+	}
+	const dec = () => {
+		props.decreaseValue()
+	}
+	const rst = () => {
+		//props.resetValue({ type: RESET })
+		props.resetValue(reset())
+	}
 	return (
 	 		<div>
-	 			<button>+</button><button>-</button><button>reset</button>
-	 			<p>{state}</p>
+	 			<button onClick={inc} >+</button>
+	 			<button onClick={dec} >-</button>
+	 			<button onClick={rst} >reset</button>
+	 			<p>{props.value} </p>
 	 		</div>
 		)
 }
 
 //react-redux
-const mapStateToProps = state => {
-	return {
-		state: state,
-	}
+const mapStateToProps = (state) => {
+	return { value: state}
 }
 const mapDispatchToProps = dispatch => {
 	return {
-		increaseValue: (incrementDispatch) => {
-			dispatch(incrementDispatch)
+		increaseValue: () => {
+			dispatch(increment())
 		},
+		decreaseValue: () => {
+			dispatch({ type: DECREMENT })
+		},
+		resetValue: (resetDispatch) => {
+			dispatch(resetDispatch)
+		}
 	}
 }
 
 //ahora conectamos react con redux
-const Connector = connect(mapStateToProps, mapDispatchToProps)(Comp)
-
+const Container = connect(mapStateToProps, mapDispatchToProps)(Comp)
+console.log(typeof Container)
 const Contador = () => {
-	return (
-		<Provider store={store} >
-			<Connector />
-		</Provider>
-		)
-}
+  return (
+      <Provider store={store} >
+        <Container />
+      </Provider>
+    );
+};
 export default Contador
