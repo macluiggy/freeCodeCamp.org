cc1efd1dbe039b444852f965b199792ec8a033ba
macluiggy <ferrinluiggy@gmail.com>
2021-08-24 15:47:49 -0500

    djdhdhd

--- javascript/Intermediate Algorithm Scripting/09. Missing letters.js
+++ javascript/Intermediate Algorithm Scripting/09. Missing letters.js
@@ -1,12 +1,28 @@
-function fearNotLetter(str) {
+function fearNotLetter1(str) {
   let abc = 'abcdefghijklmnopqrstuvwxyz'
-  return str
-          .split('')
-          .filter(l => {
-            console.log(abc.indexOf(l) === -1)
-          });
+  let left = abc.indexOf(str[0])
+  let right = abc.indexOf(str[str.length-1])
+  let extract = abc.slice(left, right+1)
+  console.log(left, right, extract)
+  let letter = extract.split('').filter(l => {
+    console.log(str.indexOf(l))
+    return str.indexOf(l) === -1
+  })
+  console.log(letter)
+  return letter[0]
 }
 
-console.log(fearNotLetter("abce"));
+function fearNotLetter(str) {
+  let abc = 'abcdefghijklmnopqrstuvwxyz'
+  return abc
+          .slice(
+            abc.indexOf(str[0]),
+            abc.indexOf(str[str.length-1])+1)
+          .split('')
+          .filter(l => str.indexOf(l) === -1)[0]
+}
+
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
 
 //console.log(abc.includes('abce'))
+//console.log('log message'.match())
