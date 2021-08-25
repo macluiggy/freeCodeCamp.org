c47e9f041d91f07c223434f2b8a5fdcec3e4bf34
macluiggy <ferrinluiggy@gmail.com>
2021-08-25 11:47:29 -0500

    fubonacci sussecion

--- javascript/Intermediate Algorithm Scripting/11. Convert HTML Entities.js
+++ javascript/Intermediate Algorithm Scripting/11. Convert HTML Entities.js
@@ -1,7 +1,22 @@
-function convertHTML(str) {
-  return str;
+function convertHTML1(str) {
+  return str
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g,'&apos;')
 }
+function convertHTML(str) {
+   const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str
+          .replace(/&|<|>|"|'/g, match => htmlEntities[match])
+}
+console.log(convertHTML("Dolce & Gabbana <"));
 
-convertHTML("Dolce & Gabbana");
-
-console.log('log message'.replace(/(lo)|(mes)/g, "$1xd-$2jaaa" ))
+console.log('log message'.replace(/(lo)(mes)/g, "$1xd$dos" ))
