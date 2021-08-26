5466daa9258a568ee664ef185ee21640cce61eef
macluiggy <ferrinluiggy@gmail.com>
2021-08-26 11:40:49 -0500

    binary agents

--- javascript/Intermediate Algorithm Scripting/16. Steamroller.js
+++ javascript/Intermediate Algorithm Scripting/16. Steamroller.js
@@ -1,15 +1,32 @@
 function checkIfIsArray(array) {
   // body...
-  if (!Array.isArray(array)) {
+  /*if (!Array.isArray(array)) {
     return array
   }
-  return checkIfIsArray(...array)
+  let arr = array.map(el => Array.isArray(el) ? el[0] : el)
+  console.log(arr)
+  return checkIfIsArray(...arr)*/
+  if (Array.isArray(array)) {
+    return array || array.map(x => Array.isArray(x) ? checkIfIsArray(x) : x)
+  }
+  return array || array.map(x => Array.isArray(x) ? checkIfIsArray(x) : x)
 }
 
 function steamrollArray(arr) {
-  return arr.map(el => {
-    return checkIfIsArray(el)
-  });
+  const flatArray = []
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    if (Array.isArray(el)) {
+      flatArray.push(...steamrollArray(el))
+    } else {
+      flatArray.push(el)
+    }
+  }
+  return flatArray
+}
+function steamrollArray0(arr) {
+  const flat = [].concat(...arr);
+  return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
 }
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
