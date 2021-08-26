4c94c0747b0a15c50c7b6808867fd47193d2a5f5
macluiggy <ferrinluiggy@gmail.com>
2021-08-26 13:45:57 -0500

    everething be true

---
+++ javascript/Intermediate Algorithm Scripting/18. Everything Be True.js
@@ -0,0 +1,11 @@
+function truthCheck1(collection, pre) {
+  let arr = collection.filter(obj => !!obj[pre])
+  console.log(arr)
+  return arr.length === collection.length;
+}
+function truthCheck(collection, pre) {
+  return collection
+                .filter(obj => obj[pre])
+                .length === collection.length
+}
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
