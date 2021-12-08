6b0860f49a423426607ce48b7e472bd037a920c5
macluiggy <ferrinluiggy@gmail.com>
2021-12-08 14:01:35 -0500

    practicing prototypes

--- javascript/Object Oriented Programming/12. Understand the Constructor Property.js
+++ javascript/Object Oriented Programming/12. Understand the Constructor Property.js
@@ -7,15 +7,26 @@ function Cat(name) {
 // Only change code below this line
 function joinDogFraternity(candidate) {
   let test = Dog.prototype.isPrototypeOf(candidate) // es este objeto contruido por el constructor Dog? tambie se podria utilizar instanceof o Dog.prototype.isPrototypeOf(candidate) para realiza la misma acción
-  ? `Bienvenido ${candidate.name}`
-  : `Lo sentimos, tienes que se un perro para unirte a la fraternidad`
-  return test
-  
+    ? `Bienvenido ${candidate.name}`
+    : `Lo sentimos, tienes que se un perro para unirte a la fraternidad`;
+  return test;
 }
 
-let thor = new Dog('thor');
-let mishu = new Cat('mishu');
-console.log(thor.constructor, Dog, mishu.constructor, Cat)
+let thor = new Dog("thor");
+let mishu = new Cat("mishu");
+console.log(thor.constructor, Dog, mishu.constructor, Cat);
 
-console.log(joinDogFraternity(thor))
-console.log(joinDogFraternity(mishu))
+console.log(joinDogFraternity(thor));
+console.log(joinDogFraternity(mishu));
+
+function Car() {}
+function ElectricCar() {}
+let electricCar = new ElectricCar();
+let car = new Car();
+function isElectric(car) {
+  return ElectricCar.prototype.isPrototypeOf(car)
+    ? "Charging..."
+    : "Car must be electric";
+}
+console.log(isElectric(electricCar));
+console.log(isElectric(car));
